c1a7e2ecbeadf67665ed1f0cb987ba73675ee70b
greenm01 <mason@greenm01.net>
2023-11-21 14:58:28 -0600

    added clap

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a new game at specified directory
+    New { path: Option<String> },
+    /// Run a game at specified game directory
+    Run { path: Option<String> },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::New { path } => {
+            println!("New game created at {path:?}")
+        }
+        Commands::Run { path } => {
+            println!("Running game at {path:?}")
+        }
+    }
 }
